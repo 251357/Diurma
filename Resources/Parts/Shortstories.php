a2b75cfbbe2fa063fa1b9b92e16fcb3872dc8ed9
Mike Koopmans <mike.m.koopmans@gmail.com>
2019-03-28 11:22:42 +0100

    Short stories template gemaakt

--- Resources/Parts/Shortstories.php
+++ Resources/Parts/Shortstories.php
@@ -1,3 +1,17 @@
 <div class="SScontainer">
-    <p class="SSText">Short stories     </p>
+    <a href="#"><div class="SSTextContainer"><h3 class="SSText">Kort nieuws</h3></div></a>
+
+    <a href="#"><div class="SStorie">
+        <img src="Photo/SSTorie1.png" alt="Short storie">
+        <h2>Hier artikel text - super interesant!</h2>
+    </div></a>
+    <a href="#"><div class="SStorie">
+        <img src="Photo/SSTorie2.png" alt="Short storie">
+        <h2>ook hier een iets langere text voor een artikel titel.</h2>
+    </div></a>
+    <a href="#"><div class="SStorie">
+        <img src="Photo/SSTorie3.png" alt="Short storie">
+        <h2>Nog een titel maar deze is iets langer dan de rest wegens testing purposes</h2>
+    </div></a>
+
 </div>
